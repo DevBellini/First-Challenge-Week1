72c9eebb1346e4a9533880f5c515b699dad9b1c0
Guilherme <guilherme.bellini.pb@compasso.com.br>
2023-08-18 20:46:15 -0300

    feat: final commit condition if it is different from -1

--- Feature Q-02/src/Logic.java
+++ Feature Q-02/src/Logic.java
@@ -15,7 +15,21 @@ public class Logic {
         int numUpset = 0, numFunny = 0;
 
         int i = str1.indexOf(funny);// finds the position of a value in the string. It returns the index of the first occurrence of the value, or -1
+        
+        while (i != -1) { // enters the condition if it is different from -1 and goes through the string until it finds the sad
+            numUpset++;
+            str2 = str2.substring(i + 3);
+            i = str2.indexOf(upset);
+        }
 
+        if (numFunny > numUpset)// condition if numfunny is greater than numupset returns Funny
+            System.out.println("funny");
+
+        else if (numFunny < numUpset)//condition if numfunny is less than numupset returns upset
+            System.out.println("upset");
+
+        else
+            System.out.println("neutral"); // if character is not incremented, neutral will be returned
     }
 }
 
