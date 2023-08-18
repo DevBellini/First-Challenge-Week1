40336de56f321cf0e90b3ef2a5eefb1689ddeb84
Guilherme <guilherme.bellini.pb@compasso.com.br>
2023-08-18 20:43:38 -0300

    feat: used text input and indexof

---
+++ Feature Q-02/src/Logic.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Logic {
+    public static void log() {
+
+        String funny = ":-)";
+        String upset = ":-(";
+
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine(); // text entry
+
+        String str2 = str1; // str1 Receives str2
+
+        int numUpset = 0, numFunny = 0;
+
+        int i = str1.indexOf(funny);// finds the position of a value in the string. It returns the index of the first occurrence of the value, or -1
+
+    }
+}
+
+
