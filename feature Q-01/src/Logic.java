3afee75906400f7ec2cb029d47342ccb70944093
Guilherme <guilherme.bellini.pb@compasso.com.br>
2023-08-18 16:59:45 -0300

    feat: created array to store names and problems01

--- feature Q-01/src/Logic.java
+++ feature Q-01/src/Logic.java
@@ -13,8 +13,13 @@ public class Logic {
             if (n < 1 || n > 100) { // condition from 1 to 100 students
                 System.out.println("Number of students must be between 1 to 100 students!");
                 return;
+
             }
+            String[][] students = new String[n][1]; // Creating an array with n rows and 1 column to save the student's name
+            int[][] grades = new int[n][1]; // creation of array with n rows and 1 column to save in one of problems
+
         }
     }
 }
+
 
