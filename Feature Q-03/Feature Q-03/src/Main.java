33ae69d4ecb50c68d5c62da214f5dc5a5db3ceb4
Guilherme <guilherme.bellini.pb@compasso.com.br>
2023-08-19 12:55:15 -0300

    feat: Input implementation and repeating loop

---
+++ Feature Q-03/Feature Q-03/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = null;
+
+        while (sc.hasNextLine()) { // repeating loop that receives the string, saves to list, and checks another line of text
+            s = sc.nextLine(); //string input
+        }
+    }
+}
+
